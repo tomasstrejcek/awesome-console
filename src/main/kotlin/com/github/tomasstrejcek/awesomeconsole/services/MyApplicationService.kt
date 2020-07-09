7d614c45a1c3647a7ca860e0063be4153763a6a7
GitHub Action <action@github.com>
2020-07-09 12:12:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tomasstrejcek/awesomeconsole/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tomasstrejcek.awesomeconsole.services
+
+import com.github.tomasstrejcek.awesomeconsole.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
